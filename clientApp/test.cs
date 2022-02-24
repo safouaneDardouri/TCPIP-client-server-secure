143f995885fda2000c29520a87e1b81a6beae7b2
safouaneDardouri <37278537+safouaneDardouri@users.noreply.github.com>
2022-02-24 13:36:45 +0100

    Add files via upload

---
+++ clientApp/test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ServerApp
+{
+    internal class test
+    {
+        [Fact]
+        public async Task testBidon()
+        {
+            // Arrange
+
+
+            // Act
+            var number = 1;
+
+            // Assert
+            number.Equals(Equals(1));
+        }
+    }
+}
